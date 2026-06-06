c72013893300f80acb1969010248ed479c59360d
Abel Sifuna <abelsifuna515@gmail.com>
2026-06-06 16:56:53 +0300

    review projects

--- src/db.rs
+++ src/db.rs
@@ -75,7 +75,7 @@ pub async fn create_link(
         .fetch_optional(pool)
         .await?;
         
-        let mut current = match curret {
+        let mut current = match current {
             Some(link) => link,
             None => return Ok(None),
         };
