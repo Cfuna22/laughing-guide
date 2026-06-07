6bcf576b33363eee79efe04c0e5d44ed35668dbe
Abel Sifuna <abelsifuna515@gmail.com>
2026-06-07 21:17:53 +0300

    review projects

--- src/db.rs
+++ src/db.rs
@@ -91,7 +91,7 @@ pub async fn create_link(
                 .is_some();
 
             if exists {
-                return Err(sqlx::Error::RowNotFound);
+                return Err(sqlx::Error::RowNotound);
             }
             current.slug = new_slug;
         }
