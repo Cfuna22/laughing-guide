907b0444c2dc96de99022c73bbe58d625820951f
Abel Sifuna <abelsifuna515@gmail.com>
2026-06-17 18:19:01 +0300

    changes

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use tower_http::trace::TraceLayer;
 #[derive(Clone)]
 pub struct AppState {
     db_pool: PgPool,
-    base_url: String,  // e.g., "http://localhost:3000" or "https://short.io"
+    base_url: String, 
 }
 
 #[tokio::main]
