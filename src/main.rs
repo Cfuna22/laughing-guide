f2f9019bf4127bd82e4d37e81908fac80427bc6e
Abel Sifuna <abelsifuna515@gmail.com>
2026-06-17 18:19:34 +0300

    changes

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ async fn main() {
         .with_state(state);
     
     // Start server
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.00:3000")
         .await
         .unwrap();
     
